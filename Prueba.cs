00c49362952e4f7a5da5c33e31f099fb0d7d78a7
ale <alemccr@gmail.com>
2024-11-02 20:54:12 -0600

    Se agregaron nuevas clases

---
+++ Prueba.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	public Prueba()
+	{
+		string nombre = string.Empty;
+		string apellido = string.Empty;
+		int edad = 0;
+
+		string cadena = nombre + apellido + edad;
+	}
+}
